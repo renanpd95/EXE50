77b9dbd9eee45b4a8a92cff10df12b6a7d218692
Renan <88853399+renanpd95@users.noreply.github.com>
2022-04-26 19:04:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+
+mes = int(input("Digite o mês de 1 a 12: "))
+
+os.system('clear')
+
+if(mes == 1):
+  print("Janeiro")
+elif(mes == 2):
+  print("Fevereiro")
+elif(mes == 3):
+  print("Março")
+elif(mes == 4):
+  print("Abril")
+elif(mes == 5):
+  print("Maio")
+elif(mes == 6):
+  print("Junho")
+elif(mes == 7):
+  print("Julho")
+elif(mes == 8):
+  print("Agosto")
+elif(mes == 9):
+  print("Setembro")
+elif(mes == 10):
+  print("Outubro")
+elif(mes == 11):
+  print("Novembro")
+elif(mes == 12):
+  print("Dezembro")
+elif ( mes < 1 or mes > 12 ):
+  print("Erro de digitação")
